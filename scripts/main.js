c10078819779113dbf2be32fdd51135085dc35e2
Ethan Philpott <ephilpott2004@gmail.com>
2021-03-09 00:53:03 -0700

    I got it working :/

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-import { Noise } from "./noise.js";
-
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
@@ -10,17 +8,24 @@ c.height = window.innerHeight;
 
 let imageData = ctx.createImageData(c.width, c.height);
 
-let noise = new Noise(Math.random());
+// https://github.com/josephg/noisejs
+let noise = new Noise(Date.now());
+// https://github.com/josephg/noisejs
 
-for (let y = 0; y < c.height; y++) {
-    for (let x = 0; x < c.width; x++) {
-        let value = noise.simplex2(x / 100, y / 100);
-        console.log(value);
-        // let r = Math.round(255 * value);
-        // let g = Math.round(255 * value);
-        // let b = Math.round(255 * value);
-        // let a = 255
-        // imageData.data[] =
+for (let y = 0; y < c.height * 4; y += 4) {
+    for (let x = 0; x < c.width * 4; x += 4) {
+        let value = noise.simplex2(x / (c.width * 4), y / (c.height * 4));
+        let r = Math.round(255 - 255 * (value > 0 ? 1 : 0));
+        let g = Math.round(255 - 255 * (value > 0 ? 1 : 0));
+        let b = Math.round(255 - 255 * (value > 0 ? 1 : 0));
+        let a = 255;
+        imageData.data[y * c.width + x + 0] = r;
+        imageData.data[y * c.width + x + 1] = g;
+        imageData.data[y * c.width + x + 2] = b;
+        imageData.data[y * c.width + x + 3] = a;
     }
 }
+
+console.log(imageData.data[100000]);
+
 ctx.putImageData(imageData, 0, 0);
